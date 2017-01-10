3af9fcc61dd407efa3ce0ae63a3295512c987218
Alex Lee <alexleegk@gmail.com>
2017-01-10 09:39:06 -0800

    Change hierarchy of models.tar.gz so that models.mf matches that hierarchy.

--- models/download.py
+++ models/download.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import argparse
 import urllib2
+import tarfile
 
 
 assert "CITYSIM3D_DIR" in os.environ
@@ -11,20 +12,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--rsync", action="store_true")
 args = parser.parse_args()
 
-
+local_dir = os.path.expandvars("${CITYSIM3D_DIR}")
 if args.rsync:
-    remote_dir = "/var/www/citysim3d/models/"
-    local_dir = os.path.expandvars("${CITYSIM3D_DIR}/models/")
-    subprocess.check_call("rsync -azvu pabbeel@rll.berkeley.edu:%s %s" % (remote_dir, local_dir), shell=True)
+    remote_files = "/var/www/citysim3d/models{,.mf}"
+    subprocess.check_call("rsync -azvu pabbeel@rll.berkeley.edu:%s %s" % (remote_files, local_dir), shell=True)
 else:
     print "downloading tar file (this might take a while)"
     remote_fname = "http://rll.berkeley.edu/citysim3d/models.tar.gz"
-    local_dir = os.path.expandvars("${CITYSIM3D_DIR}/models/")
     local_fname = os.path.join(local_dir, "models.tar.gz")
     urlinfo = urllib2.urlopen(remote_fname)
     with open(local_fname, "w") as fh:
         fh.write(urlinfo.read())
 
     print "unpacking file"
-    # unpack without top-level directory
-    subprocess.check_call("tar -xvf %s -C %s --strip-components 1" % (local_fname, local_dir), shell=True)
+    with tarfile.open(local_fname) as tar:
+        tar.extractall(local_dir)
